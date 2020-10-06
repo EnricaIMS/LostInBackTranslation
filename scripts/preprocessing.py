4687bf3f9a0b8e6f4da1192281323601ee1f2950
Enrica Troiano <enrica.troiano@gmail.com>
2020-10-06 12:26:55 +0200

    Changed preprocessing.py

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -18,20 +18,22 @@ def convert_data(File, goal):
     myFile.columns = colsINPUT
 
     # extract emotion labels
-    emotions = list(set(map(str.strip, myFile['EmotionLabel'].str.lower())))
+    emotions = list(set(map(str.strip, myFile["EmotionLabel"].str.lower())))
     
     # insert column in file, depending on goal
     if goal == "Simple_Translation":
-        target_emotion=['None']
-    elif goal == "Recover_Emotion":
-        print('ambe')    
-    else:
-        print('UAGLIO')
-    new_column = target_emotion*len(myFile)
-    myFile.insert(2, "Target_Emotion", new_column, allow_duplicates = False)
+        new_column = ['None']*len(myFile)
+
+    elif goal == "Restore_Overgeneration":
+        new_column = myFile["EmotionLabel"] #same as input emotion
     
+    else:
+        # extract emotion labels and use them as target emotions
+        emotions = list(set(map(str.strip, myFile["EmotionLabel"].str.lower())))
+        new_column = [', '.join(emotions)]*len(myFile)
+        
+    myFile.insert(2, "Target_Emotion", new_column, allow_duplicates = False)
     myFile.to_csv(os.path.splitext(sys.argv[1])[0] + "_preprocessed.txt", index=False, sep="\t", header= None)
-    print("{} : {} : STATUS : Preprocessed data have been saved in ./output/".format(script, datetime.datetime.now()))
 
 if __name__ == "__main__":
     
@@ -40,4 +42,4 @@ if __name__ == "__main__":
     goal = configParser.get("Goal", "goal")
 
     convert_data(sys.argv[1], goal)
-    
+    print("{} : {} : STATUS : Preprocessed data have been saved in ./output/".format(script, datetime.datetime.now()))
