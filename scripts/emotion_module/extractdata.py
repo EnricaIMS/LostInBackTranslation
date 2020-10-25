699a0802285848e827955f31a0703aa692cc59db
Enrica Troiano <enrica.troiano@ims.uni-stuttgart.de>
2020-10-25 19:24:41 +0100

    Modified extractdata

--- scripts/emotion_module/extractdata.py
+++ scripts/emotion_module/extractdata.py
@@ -1,9 +1,13 @@
 from emotion_module.objective.emocl.ued_handler import UEDDataset, UEDLoader 
-import random, sys
-    
-datasets=['isear','tec','blogs','tales']
+import random, sys, datetime
 
+script='./emotion_module/extractdata.py'
+
+datasets=['isear','tec','blogs','tales']
+loader = UEDLoader('/home/users2/troianea/Projekte/projectdata/emotion_classification/LexicalSubstitution4StyleTransfer/David/emotion-transfer/datasets/unified-dataset.jsonl')
+   
 for name in datasets:
+    print("{} : {} : STATUS : Extracting {} data from ../data/unified-corpora".format(script, datetime.datetime.now(),name))
 
     corpus=name.strip()
     if corpus=='blogs':
@@ -12,7 +16,6 @@ for name in datasets:
         corpus='tales-emotion'
     
 
-    loader = UEDLoader('/home/users2/troianea/Projekte/projectdata/emotion_classification/LexicalSubstitution4StyleTransfer/David/emotion-transfer/datasets/unified-dataset.jsonl')
     filtered_dataset = loader.filter_datasets(source=corpus)
 
     random_seed=42
