b2673f204cc5f4cfce26c5df37f6aac19e142bf5
Enrica Troiano <enrica.troiano@ims.uni-stuttgart.de>
2020-10-25 18:09:29 +0100

    Scripts to extractdata

--- scripts/emotion_module/preprocessing.py
+++ scripts/emotion_module/preprocessing.py
@@ -1,4 +1,23 @@
 import spacy
+from abc import ABC, abstractmethod
+
+import json
+
+
+class Dataset(ABC):
+    """ Base class for handling emotion datasets """
+
+    @abstractmethod
+    def make_iterator(self):
+        """ This method should return an iterator over all instances in the dataset """
+        pass
+
+    @abstractmethod
+    def split(self):
+        """ This method should split the given dataset and return new Dataset instances according
+        to a pre-defined split ratio """
+        pass
+
 
 class SpacyPreprocessor:
 
